008f02724b202a2fd93b460c61ba4af480070df5
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-05-05 10:11:53 -0700

    Completed my cli based atm machine

---
+++ main.ts
@@ -0,0 +1,74 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+let myBalance = 10000; //dollar
+console.log(`your curent balance is ${myBalance}`);
+let pinCode = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name:"pin",
+        message: "Enter your pin number",
+        type: "number",
+    },
+]);
+if (pinAnswer.pin === pinCode) {
+    console.log("Correct pin number!!!");
+
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Select an option",
+            type: "list",
+            choices: ["withdraw", "check balance" , "fast cash"],
+        },
+    ]);
+    console.log(operationAns);
+
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter amount to withdraw",
+                type: "number",
+            },
+        ]);
+        if (amountAns.amount > myBalance) {
+            console.log("Insufficient balance");
+            
+        }
+
+        //Using Assignment operator -=
+
+        else {
+            let answer = myBalance -= amountAns.amount;
+        console.log(`your remaining balance is ${answer} `);
+        }
+    } else if (operationAns.operation === "check balance"){
+        console.log(`your balance is ${myBalance}`);
+
+    }
+
+    else if (operationAns.operation === "fast cash"){
+        let fastCash = await inquirer.prompt (
+            [
+                {
+                    name:"cash",
+                    message:"Enter the amount to withdrawl",
+                    type:"list",
+                    choices:[1000,2000,5000,7000,10000]
+
+                }
+            ]
+        );
+        myBalance -= fastCash.cash;
+        console.log(` you have succesfully withdrawl ${fastCash.cash} \n your remaining balance is ${myBalance}`);
+        
+    }
+
+}
+
+else {
+    console.log("Incorrect pin number");
+}
