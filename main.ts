16474b0913aa0394c758f1bc72a68d15e4aa5f89
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-05-06 08:08:09 -0700

    added a note

--- main.ts
+++ main.ts
@@ -72,3 +72,7 @@ if (pinAnswer.pin === pinCode) {
 else {
     console.log("Incorrect pin number");
 }
+//FOR THE TEACHER :
+// this project is only made by arisha Ghaffar.
+//me and my sister both uses the same laptop thats why 
+//whenever we upload our projects on github it shows that this is made by both of us.
